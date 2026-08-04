046d36b1a0219cd4ef2d03940cee34835d8d71e0
B Vikas rao <vikasrao.banala@gmail.com>
2026-08-04 12:21:19 +0530

    Delete DAY-8_NUMPY directory

--- DAY-8_NUMPY/10_array_split.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-a=np.array([1,2,3,4,5,6])
-print(np.array_split(a,3))
-b=np.array([[1,2],[3,4],[5,6],[7,8]])
-print(np.array_split(b,2))
-c=np.array([[1,2,3,4],[5,6,7,8]])
-print(np.hsplit(c,2))
-d=np.array([[1,2],[3,4],[5,6],[7,8]])
-print(np.vsplit(d,2))
