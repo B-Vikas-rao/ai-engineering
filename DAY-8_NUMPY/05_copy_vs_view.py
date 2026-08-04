b9a0105ade711d3e844e0ce6eac5b1953f43c6ee
B_Vikas_rao <vikasrao.banala@gmail.com>
2026-08-04 11:50:12 +0530

    Day-8 NUmpy progrma from (1-5)

---
+++ DAY-8_NUMPY/05_copy_vs_view.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = np.array([10, 20, 30, 40])
+b = a.copy()
+c = a.view()
+a[0] = 100
+print(a)
+print(b)
+print(c)
+c[1] = 200
+print(a)
+print(b)
+print(c)
+print(a.base)
+print(b.base)
+print(c.base)
